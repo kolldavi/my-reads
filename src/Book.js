5b42681bdaebbf23e9b2cf884d5cdd55ebd7d019
kolldavi <kolldavi@gmail.com>
2017-08-22 11:21:16 -0500

    added modal for books

--- src/Book.js
+++ src/Book.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import * as BooksAPI from './BooksAPI';
+import ReactModal from 'react-modal';
 
 class Book extends Component {
   //assign required prop-types to book component
@@ -8,7 +9,9 @@ class Book extends Component {
     book: PropTypes.object.isRequired,
     onMoveBook: PropTypes.func.isRequired
   };
-
+  state = {
+    open: false
+  };
   //updateBook using BookAPI
   updateBook = event => {
     const { book, onMoveBook } = this.props;
@@ -16,6 +19,15 @@ class Book extends Component {
     BooksAPI.update(book, shelf).then(() => {
       onMoveBook(book, shelf);
     });
+  };
+
+  // called when user hovers book cover thumbnail image and clicks to display modal with more info
+  toggleModal = () => {
+    if (this.state.displayModal) {
+      this.setState({ displayModal: false });
+    } else {
+      this.setState({ displayModal: true });
+    }
   };
 
   //render each book
@@ -26,7 +38,7 @@ class Book extends Component {
     let thumbnail = book.imageLinks
       ? book.imageLinks.thumbnail
       : 'https://books.google.com/googlebooks/images/no_cover_thumb.gif';
-    let authors = book.authors ? book.authors : [];
+    let authors = book.authors ? book.authors : 'unknown';
 
     return (
       <div className="book">
@@ -38,7 +50,13 @@ class Book extends Component {
               height: 193,
               backgroundImage: `url(${thumbnail})`
             }}
-          />
+          >
+            <div className="book-overlay" onClick={this.toggleModal}>
+              <div className="get-more-info">
+                <h3>more</h3>
+              </div>
+            </div>
+          </div>
           <div className="book-shelf-changer">
             <select value={book.shelf} onChange={this.updateBook}>
               <option value="none" disabled>
@@ -57,6 +75,54 @@ class Book extends Component {
         <div className="book-authors">
           {authors}
         </div>
+        <ReactModal
+          onRequestClose={this.toggleModal}
+          isOpen={this.state.displayModal}
+          contentLabel="Modal"
+        >
+          <h1 className="modal-header">
+            {book.title}
+          </h1>
+          <div className="modal-book-info">
+            <div
+              className="modal-book-cover"
+              style={{
+                backgroundImage: `url('${thumbnail}')`,
+                width: 128,
+                height: 193
+              }}
+            />
+            <div>
+              <h3 className="modal-header">Published By</h3>
+              <p className="modal-detail">
+                {book.publisher}
+              </p>
+            </div>
+            <div>
+              <h3 className="modal-header">Date Published</h3>
+              <p className="modal-detail">
+                {book.publishedDate}
+              </p>
+            </div>
+            <div>
+              <h3 className="modal-header">Author(s)</h3>
+              <p className="modal-detail">
+                {authors}
+              </p>
+            </div>
+          </div>
+          <div className="modal-description-container">
+            <h3 className="modal-description-detail-header">Description</h3>
+            <p className="modal-book-detail">
+              {book.description}
+            </p>
+          </div>
+          <div className="close-button-container">
+            <button className="close-modal-button" onClick={this.toggleModal}>
+              close
+            </button>
+          </div>
+        </ReactModal>
       </div>
     );
   }
